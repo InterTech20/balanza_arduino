6984463f239d399ae2aeef7b9c7aaec15d0cf00c
Jhonathan O'Brian Saa Se <64106872+InterTech20@users.noreply.github.com>
2021-04-30 02:23:55 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,13 +4,61 @@ const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
-
 app.use(express.static(__dirname + '/public'));
 server.listen(3000, () => console.log('server on port 3000'));
+//------------------------------------
+app.use(express.static("static"));
+/*============================================================== */
+const mysql = require('mysql');
 
+function guardar_bd(peso) {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'balanza'
+ });
+ 
+ connection.connect(function(error){
+   try{ 
+ 
+     if(error){ 
+         console.log("Error al establecer la conexión a la BD -- " + error); 
+     }else{  
+         console.log("Conexión exitosa"); 
+     } 
+ }
+ catch(x){ 
+     console.log("Contacto.agregarUsuario.connect --Error-- " + x); 
+ }
+ });
 
-// const io = require('socket.io')(server);
+ const query = connection.query('INSERT INTO peso(peso) VALUES( ?)',
+ [peso]
+ ,
+  function(error, result){
+   if(error){
+      throw error;
+   }else{
+      console.log(result);
+   }
+ }
+ );
+ 
+ connection.end();
+}
+/*============================================================== */
 
+/*======*/
+io.on('connection', (socket) =>{
+  
+        socket.on('message', (data)=>{ 
+        io.sockets.emit('message', data);
+        guardar_bd(data);   
+        });
+
+});
+/*======*/
 
 const SerialPort = require('serialport');
 const ReadLine = SerialPort.parsers.Readline;
@@ -26,10 +74,12 @@ parser.on('open', function () {
 
 parser.on('data', function (data) {
   console.log(data);
-io.emit('balanza',data.toString());
+ io.emit('data',data);
 });
+
 
 parser.on('error', (err) => console.log(err));
 port.on('error', (err) => console.log(err));
+
 
 
